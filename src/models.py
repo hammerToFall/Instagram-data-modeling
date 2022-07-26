168cf29e7f5165c40b3d1180e97e130ea993050f
hammerToFall <lenorka0@hotmail.com>
2022-07-26 12:49:36 +0000

    añado cambios

--- src/models.py
+++ src/models.py
@@ -12,29 +12,34 @@ class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
 
-class Publish(Base):
-    __tablename__ = 'publish'
+class Media(Base):
+    __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    post = Column(String(250), ForeignKey('user.id'))
-    story = Column(String(250))
-    reel = Column(String(250), nullable=False)
-   # person = relationship(Person)
+    type = Column(String(250))
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
 
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250), ForeignKey('publish.post'))
-    autor_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, nullable=False)
+    comment_text = Column(String(250), ForeignKey('Media.post_id'))
+    user_id = Column(Integer, ForeignKey('User.id'))
+    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
 
 class Likes(Base):
     __tablename__ = 'likes'
     id = Column(Integer, primary_key=True)
-    likes_num = Column(Integer, ForeignKey('publish.reel'))
+    post_id = Column(Integer, ForeignKey('Post.id'))
+    user_id = Column(Integer, ForeignKey('User.id'))
 
 
 
